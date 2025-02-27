d261d1de55e9308c3ac2bea901ec0c235fe0da80
Nsvoriginals <123223662+nsvoriginals@users.noreply.github.com>
2025-02-27 19:02:40 +0530

    added all files

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 import WebSocket,{ WebSocketServer } from "ws"
 const wss=new WebSocketServer({port:8080})
 
-wss.on("connection",(ws:WebSocket)=>{
+wss.on("connection",(wss)=>{
     console.log('New client connected');
     ws.on("message",(message:string)=>{
        
